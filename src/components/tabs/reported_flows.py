6d94b6da9dfb6777b5918056b0f8f84472451d8d
MooSee2 <52461645+MooSee2@users.noreply.github.com>
2024-03-28 22:49:35 -0600

    fixed reported flows

--- src/components/tabs/reported_flows.py
+++ src/components/tabs/reported_flows.py
@@ -7,51 +7,71 @@ reported_flows = [
         "Boundary Reservoir Pipeline",
         "pipeline-input",
         "Box 5b: DAM3",
+        "box5b-tip",
+        "",
     ),
     (
         "Long Creek Minor Project Diversion",
         "long-creek-minor-project-diversion-input",
         "Box 11: DAM3",
+        "box11-tip",
+        "",
     ),
     (
         "US Diversion",
         "us-diversion-input",
         "Box 12: DAM3",
+        "box12-tip",
+        "",
     ),
     (
         "City of Weyburn Pumpage",
         "weyburn-pumpage-input",
         "Box 16: DAM3",
+        "box16-tip",
+        "",
     ),
     (
         "City of Weyburn Return Flow",
         "weyburn-return-flow-input",
         "Box 18: DAM3",
+        "box18-tip",
+        "",
     ),
     (
         "Upper Souris Minor Project Diversion",
         "upper-souris-minor-diversion-input",
         "Box 25: DAM3",
+        "box25-tip",
+        "",
     ),
     (
         "City of Estavan Net Pumpage",
         "estevan-net-pumpage-input",
         "Box 27: DAM3",
+        "box27-tip",
+        "",
     ),
     (
         "Short Creek Diversions",
         "short-creek-diversions-input",
         "Box 28: DAM3",
+        "box28-tip",
+        "",
     ),
     (
         "Lower Souris Minor Project Diversion",
         "lower-souris-minor-diversion-input",
         "Box 29: DAM3",
+        "box29-tip",
+        "",
     ),
     (
         "Moose Mountain Minor Project Diversion",
         "moose-mountain-minor-diversion-input",
         "Box 37: DAM3",
+        "box37-tip",
+        "",
     ),
 ]
 
@@ -63,27 +83,27 @@ reported_flows_container = [
             html.Div(
                 className="reported-flows-info-container",
                 children=[
-                    html.H6(name),
+                    html.H6(box_name),
                     html.Div(
-                        id="appor-end-tip",
+                        id=tip_name,
                         className="info-container",
                         children=html.Div(className="fa-solid fa-info"),
                     ),
                 ],
             ),
             dbc.Tooltip(
-                "The end date for apportionment. By default, December-31th.",
-                target="appor-end-tip",
+                tip_text,
+                target=tip_name,
             ),
             dbc.Input(
-                id=id,
+                id=container_id,
                 class_name="reported-flows-input",
-                placeholder=box,
+                placeholder=box_label,
                 type="number",
             ),
         ],
     )
-    for name, id, box in reported_flows
+    for box_name, container_id, box_label, tip_name, tip_text, in reported_flows
 ]
 
 # children = []
