f07bca3b134d3ae2fab4afe0b2ea3e95ebb26a4f
Jonathan O'Connell <joconnell@usgs.gov>
2024-04-11 10:22:12 -0600

    remove dummy data

--- src/modules/data_layer/data_layer.py
+++ src/modules/data_layer/data_layer.py
@@ -1,45 +1,10 @@
 import app_data.test_data as td
+from modules.data_layer import nwis
+from modules.data_layer import wo_realtime as wo
 
 
 def get_app_data(apportionment_year: int) -> tuple:
-    return (
-        td.make_dummy_data(
-            stations={
-                "05NA006",
-                "05NB020",
-                "05NB016",
-                "05NC002",
-                "05ND012",
-            }
-        ).to_dict("records"),
-        td.make_dummy_data(
-            stations={
-                "05NB001",
-                "05NB036",
-                "05NB011",
-                "05NB018",
-                "05NA003",
-                "05NB040",
-                "05NB041",
-                "05NB038",
-                "05NB014",
-                "05NB035",
-                "05NB033",
-                "05NB039",
-            },
-        ).to_dict("records"),
-        td.make_dummy_data(
-            stations={
-                "1234_wind_speed",
-                "1234_air_temp",
-                "1234_sol_rad",
-                "1234_rel_humidity",
-                "1234_precip",
-                "4321_wind_speed",
-                "4321_air_temp",
-                "4321_sol_rad",
-                "4321_rel_humidity",
-                "4321_precip",
-            }
-        ).to_dict("records"),
-    )
+    wo_realtime_reservoirs = wo.get_wo_realtime_reservoirs(apportionment_year)
+    wo_realtime_discharge = wo.get_wo_realtime_discharge(apportionment_year)
+    nwis_data = nwis.get_nwis_data(apportionment_year)
+    return wo_realtime_reservoirs, wo_realtime_discharge, nwis_data
