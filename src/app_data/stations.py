8e556303ed9c9669eef214b11ed36600dec4095b
Jonathan O'Connell <joconnell@usgs.gov>
2024-04-13 23:25:01 -0600

    datetime is not editable

--- src/app_data/stations.py
+++ src/app_data/stations.py
@@ -41,7 +41,7 @@ stations = {
 
 # TODO finish approvals for all stations
 discharge_station_names = [
-    {"name": ["Discharge", " ", " ", "Date"], "id": "date"},
+    {"editable": False, "name": ["Discharge", " ", " ", "Date"], "id": "date"},
     {"name": ["Discharge", "Units: m\u00b3/s", "Long Creek near Estevan", "05NB001"], "id": "05NB001"},
     {"name": ["Discharge", "Units: m\u00b3/s", "Long Creek near Estevan", "05NB001"], "id": "05NB001_approval"},
     {"name": ["Discharge", "Units: m\u00b3/s", "Souris River below Rafterty", "05NB036"], "id": "05NB036"},
@@ -84,7 +84,7 @@ reservoir_station_names = [
 ]
 
 met_station_names = [
-    {"name": ["Roughbark", " ", "Date"], "id": "date"},
+    {"editable": False, "name": ["Roughbark", " ", "Date"], "id": "date"},
     {"name": ["Roughbark", "Wind Speed", "m/s"], "id": "1234_wind_speed"},
     {"name": ["Roughbark", "Wind Speed", "m/s"], "id": "1234_wind_speed_approval"},
     {"name": ["Roughbark", "Air Temperature", "C"], "id": "1234_air_temp"},
