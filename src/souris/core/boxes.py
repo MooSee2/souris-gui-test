3da918e33dc2835b7b0069fecd91d0cfb9e0d53b
Jonathan O'Connell <joconnell@usgs.gov>
2024-04-19 17:56:25 -0600

    Initial commit

---
+++ src/souris/core/boxes.py
@@ -0,0 +1,62 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class Boxes:
+    """This class houses the reported flows as boxes for calculations."""
+
+    box_1: Optional[int] = None
+    box_2: Optional[int] = None
+    box_3: Optional[int] = None
+    box_4: Optional[int] = None
+    box_5a: Optional[int] = None
+    box_5b: Optional[int] = None
+    box_6: Optional[int] = None
+    box_7: Optional[int] = None
+    box_8: Optional[int] = None
+    box_9: Optional[int] = None
+    box_10: Optional[int] = None
+    box_11: Optional[int] = None
+    box_12: Optional[int] = None
+    box_13: Optional[int] = None
+    box_14: Optional[int] = None
+    box_15: Optional[int] = None
+    box_16: Optional[int] = None
+    box_17: Optional[int] = None
+    box_18: Optional[int] = None
+    box_19: Optional[int] = None
+    box_20: Optional[int] = None
+    box_21: Optional[int] = None
+    box_22: Optional[int] = None
+    box_23a: Optional[int] = None
+    box_23b: Optional[int] = None
+    box_24: Optional[int] = None
+    box_25: Optional[int] = None
+    box_26: Optional[int] = None
+    box_27: Optional[int] = None
+    box_28: Optional[int] = None
+    box_29: Optional[int] = None
+    box_30: Optional[int] = None
+    box_31: Optional[int] = None
+    box_32: Optional[int] = None
+    box_33: Optional[int] = None
+    box_34: Optional[int] = None
+    box_35: Optional[int] = None
+    box_36: Optional[int] = None
+    box_37: Optional[int] = None
+    box_38: Optional[int] = None
+    box_39: Optional[int] = None
+    box_40: Optional[int] = None
+    box_41: Optional[int] = None
+    box_42: Optional[int] = None
+    box_43: Optional[int] = None
+    box_44: Optional[int] = None
+    box_45a: Optional[int] = None
+    box_45b: Optional[int] = None
+    box_46: Optional[int] = None
+    box_47a: Optional[int] = None
+    box_47b: Optional[int] = None
+    box_48: Optional[int] = None
+    box_49: Optional[int] = None
+    box_50: Optional[int] = None
