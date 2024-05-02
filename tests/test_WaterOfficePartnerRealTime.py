e2b74ae086feeaa83755edd854263b5a663a839d
Jonathan O'Connell <joconnell@usgs.gov>
2024-05-02 09:32:15 -0600

    hmmm

--- tests/test_WaterOfficePartnerRealTime.py
+++ tests/test_WaterOfficePartnerRealTime.py
@@ -47,23 +47,43 @@ def test_get():
     assert data["05NCM01"].shape[0] == 480
 
 
-met_codes = {
-    35: "wind_speed",
-    1: "air_temp",
-    19: "precip",
-    27: "sol_rad",
-    28: "rel_humidity",
-}
+# met_codes = {
+#     35: "wind_speed",
+#     1: "air_temp",
+#     19: "precip",
+#     27: "sol_rad",
+#     28: "rel_humidity",
+# }
 
 
-def process_met_data(data: dict[str : pd.DataFrame]) -> pd.DataFrame:
-    grouped_data = data["05NB016"].groupby("parameter")
-    renamed_columns = {station: group.rename(columns={"value": station, "approval": f"{station}_approval"}) for station, group in grouped_data}
-    drop_columns = {station: group.drop(columns=["parameter", "qualifiers", "symbol"], axis=1) for station, group in renamed_columns.items()}
-    return
+# def process_met_data(data: dict[str : pd.DataFrame]) -> pd.DataFrame:
+#     grouped_data = data["05NB016"].groupby("parameter")
+#     renamed_columns = {station: group.rename(columns={"value": station, "approval": f"{station}_approval"}) for station, group in grouped_data}
+#     drop_columns = {station: group.drop(columns=["parameter", "qualifiers", "symbol"], axis=1) for station, group in renamed_columns.items()}
+#     return
 
 
-def test_process():
+# def test_process():
+#     load_dotenv("tests/.env")
+#     partner_realtime = WaterOfficePartnerRealTime(
+#         username=os.getenv("WORT_USERNAME"),
+#         password=os.getenv("WORT_PASSWORD"),
+#     )
+
+#     data = partner_realtime.get(
+#         params={
+#             "stations[]": ["05NB016"],
+#             "start_date": "2023-05-01%2000:00:00",
+#             "end_date": "2023-05-05%2023:59:59",
+#             "parameters[]": ["35", "1", "19", "27", "28"],
+#         }
+#     )
+
+#     processed = process_met_data(data)
+#     ...
+
+
+def test_get_05NB036():
     load_dotenv("tests/.env")
     partner_realtime = WaterOfficePartnerRealTime(
         username=os.getenv("WORT_USERNAME"),
@@ -72,12 +92,10 @@ def test_process():
 
     data = partner_realtime.get(
         params={
-            "stations[]": ["05NB016"],
+            "stations[]": "05NB036",
             "start_date": "2023-05-01%2000:00:00",
             "end_date": "2023-05-05%2023:59:59",
-            "parameters[]": ["35", "1", "19", "27", "28"],
+            "parameters[]": 6,
         }
     )
-
-    processed = process_met_data(data)
     ...
